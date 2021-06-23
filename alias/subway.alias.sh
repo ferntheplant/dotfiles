01c570c4a13690794b932602766258c7ee2fc7f9
Fern Sanchez <fjorn@Ferns-MacBook-Pro.local>
2021-06-22 22:00:13 -0400

    Fixed subway container aliases. TODO: run subway community setup based on system OS

--- alias/subway.alias.sh
+++ alias/subway.alias.sh
@@ -1,8 +1,8 @@
 export SUBWAY_REDIS_HOST=localhost
 export SUBWAY_DB_HOST=localhost
 
-alias startmongo="docker run -d --rm --name mongoContainer -p 27017-27019:27017-27019 -v /home/fjorn/mongo-volume:/data/db mongo:4.2.3-bionic"
-alias startredis="docker run -d --rm --name redisContainer -p 6379:6379 -v /home/fjorn/redis-volume:/data -t redis:5.0.8"
+alias startmongo="docker run -d --rm --name mongoContainer -p 27017-27019:27017-27019 -v $HOME/upchieve/mongo-volume:/data/db mongo:4.2.3-bionic"
+alias startredis="docker run -d --rm --name redisContainer -p 6379:6379 -v $HOME/upchieve/redis-volume:/data -t redis:5.0.8"
 alias killmongo="docker ps -q --filter 'name=mongoContainer' | grep -q . && docker stop mongoContainer"
 alias killredis="docker ps -q --filter 'name=redisContainer' | grep -q . && docker stop redisContainer"
 
