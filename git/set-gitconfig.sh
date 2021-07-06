6ad53d341843406f1aca8b99b306699c498085bf
Fern Sanchez <fjorn@dev.us-east4-b.c.careful-prism-317920.internal>
2021-07-06 14:33:51 +0000

    Fix git aliases

--- git/set-gitconfig.sh
+++ git/set-gitconfig.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env zsh
 
-echo $(git config --global --get user.name)
-
 git config --global user.name "Fern Sanchez"
 
 git config --global core.editor vim
@@ -9,8 +7,6 @@ git config --global credential.helper store
 
 git config --global alias.co checkout
 git config --global alias.st status
-
-echo 'memes'
 
 if [[ -z $(git config --global --get user.email) ]]; then
   echo -e "!!!\n\nGit Email not set, please configure!\n\ngit config --global user.email 'test@email.com'\n\n!!!"
