399101e85b5b51d515c9ee1e9d5b23e1633c93db
Fern Sanchez <fernando.sanchez@upchieve.org>
2021-09-01 21:03:46 +0000

    Added git alias

--- git/set-gitconfig.sh
+++ git/set-gitconfig.sh
@@ -7,6 +7,7 @@ git config --global credential.helper store
 
 git config --global alias.co checkout
 git config --global alias.st status
+git config --global alias.cm commit
 
 if [[ -z $(git config --global --get user.email) ]]; then
   echo -e "!!!\n\nGit Email not set, please configure!\n\ngit config --global user.email 'test@email.com'\n\n!!!"
